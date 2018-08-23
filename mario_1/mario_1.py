8792031baba0895d1a37ebd861a194c76249b311
michalpom <michalpom@gmail.com>
2018-08-23 12:59:16 +0200

    mario one-side pyramid

--- mario_1/mario_1.py
+++ mario_1/mario_1.py
@@ -1,1 +1,26 @@
 
+def main():
+    
+    print("The program draws Mario's one-side pyramid\n")
+    # n=input("What is the high of the pyramid? : ")
+    #print("What is the high of the pyramid?")
+
+    while True:
+        n=input("What is the high of the pyramid? :")
+        #
+        if n.isnumeric():
+            n=int(n)
+            if n > 0 and n<24:
+                break
+    print("\nThe high is " + str(n) + " and this is how it looks:\n")
+
+    level=n
+
+    for i in range(n):
+        space=" " * (level-1)
+        brick= "#" * (i+2)
+        print(space + brick)
+        level=level-1
+
+if __name__ == "__main__":
+    main()
